6ea71928f581adc1029d4aa424b35c1f68cd44d0
venky-at-034995048232 <venkatesh.yanamaddi3@gmail.com>
2025-02-27 23:33:09 +0530

    ousanding anlysus updated

--- controllers/oustandingAnalysis.js
+++ controllers/oustandingAnalysis.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Outstanding = require("../models/outstanding");
 
 const outstandingReport = asyncHandler(async (req, res) => {
-    const { licenseNo } = req.query;  // Extract the license number from query params
+    const { licenseNo } = req.query;
 
     if (!licenseNo) {
         return res.status(400).json({ message: "License number is required" });
@@ -10,18 +10,15 @@ const outstandingReport = asyncHandler(async (req, res) => {
 
     const result = await Outstanding.aggregate([
         { $match: { "uploadData.DLNo1": licenseNo } },
-        // { $unwind: "$uploadData" },
-        // { $match: { "uploadData.DLNo1": licenseNo } },
         { $project: { _id: 0, data: "$uploadData" } }
     ]);
 
     if (!result || result.length === 0) {
         return res.status(404).json({ message: "No data found" });
     }
-
     
-    
-    res.json(result[0]);
+    // Make sure we're sending the response in the expected format
+    res.json({ data: result[0].data });
 });
 
 module.exports = { outstandingReport };
